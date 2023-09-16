c3250e3f8e05d5266bf60092d2141b9099130d77
AwSick <vitja1234@gmail.com>
2023-09-16 03:36:39 +0300

    tg-bot

---
+++ index.js
@@ -0,0 +1,48 @@
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const cors = require('cors');
+
+const token = '5887076660:AAErITt-OyVu620SYG1R8I8ffOp_kyYPhi8';
+const webAppUrl = 'https://bespoke-blini-ca4c6a.netlify.app';
+
+const bot = new TelegramBot(token, {polling: true});
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if(text === '/start') {
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, выбери блюдо', {
+            reply_markup: {
+                keyboard: [
+                    [{text: 'Выбрать блюдо', web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
+    }
+});
+
+app.post('/web-data', async (req, res) => {
+    const {queryId, products = []} = req.body;
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Cписок покупок',
+            input_message_content: {
+                message_text: `${products.map(item => `Блюдо ${item.title} Продукты: ${item.products.map((item => item.join(',')))}`)}`
+            }
+        })
+        return res.status(200).json({});
+    } catch (e) {
+        return res.status(500).json({})
+    }
+})
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
