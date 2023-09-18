80326bcdd3470666c10ae3741802067223ac7b7c
AwSicker <vitja1234@gmail.com>
2023-09-18 13:01:05 +0300

    bot-tele

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ const bot = new TelegramBot(token, {polling: true});
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors());
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
